3f7b027ceb56b54979c51a02e0959e59e27d2d7e
hanseonghye <hans12312@naver.com>
2018-11-19 10:14:33 +0900

    add downstream

--- androidClient/app/src/main/java/com/example/han/myapplication/MainActivity.java
+++ androidClient/app/src/main/java/com/example/han/myapplication/MainActivity.java
@@ -5,24 +5,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.RemoteMessage;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG="myFcm";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().subscribeToTopic("notice");
+        String token=FirebaseInstanceId.getInstance().getToken();
+        Log.e(TAG,token); //user deviceID
     }
 
     public void click(View v){
-        Log.e("click","start send");
         AtomicInteger msgId=new AtomicInteger();
         FirebaseMessaging fm=FirebaseMessaging.getInstance();
-        fm.send(new RemoteMessage.Builder("your sender key" + "@gcm.googleapis.com")
+        fm.send(new RemoteMessage.Builder("your sender ID" + "@gcm.googleapis.com") //TODO input your FCM senderId
                 .setMessageId(Integer.toString(msgId.incrementAndGet()))
                 .addData("msg1", "Hello World")
                 .addData("msg2","Hello fcm")
